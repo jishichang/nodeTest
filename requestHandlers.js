4d6c812aac40c7620e456b686f8f232a8ced5a14
jishichang <jishichang@qq.com>
2016-01-20 18:00:56 +0800

    upImage

--- requestHandlers.js
+++ requestHandlers.js
@@ -32,7 +32,7 @@ function upload(response, request) {
   form.parse(request, function(error, fields, files) {
     console.log("parsing done");
     fs.renameSync(files.upload.path, "/tmp/test.png");
-    response.writeHead(200, {"Content-Type": "text/html"});
+    response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("received image:<br/>");
     response.write("<img src='/show' />");
     response.end();
